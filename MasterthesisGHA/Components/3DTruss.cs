2eb1c2c26e6f70cd4660b72f3e0be80c88dd2319
mwh <mwh@DESKTOP-PTB9NLV>
2022-02-04 17:37:27 +0100

    Add project files.

---
+++ MasterthesisGHA/Components/3DTruss.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MasterthesisGHA
+{
+    public class Truss3D : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public Truss3D()
+          : base("3D Truss Analysis", "3D Truss",
+              "Minimal Finite Element Analysis of 3D Truss",
+              "Master", "FEA")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Properties.Resources._3D_Truss_Icon;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("DC35EDA8-72CC-45ED-A755-DF28A9EFB877"); }
+        }
+    }
+}

0e81cb437b56d99f7dda3fc6c9a7c3ccaf65a29a
mwh <mwh@DESKTOP-PTB9NLV>
2022-02-04 17:37:27 +0100

    Add project files.

---
+++ MasterthesisGHA/Class Definitions/TrussModel2D.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+using MathNet.Numerics.LinearAlgebra;
+
+
+
+
+namespace MasterthesisGHA
+{
+    internal class TrussModel2D
+    {
+        public readonly List<Element> Elements;
+        public readonly List<Point3d> Nodes;
+
+        public Matrix<double> K;
+        public Vector<double> R0;
+        public Vector<double> r;
+        public Vector<double> N;
+
+        public Matrix K_out;
+        public Matrix r_out;
+        public List<double> N_out;
+
+        
+
+
+        public TrussModel2D(List<Line> lines, List<double> A, List<Point3d> anchoredPoints, List<double> loadList, List<Vector3d> loadVecs, double E )
+        {
+            if (lines.Count == 0)
+                throw new Exception("Line Input is not valid!");
+
+            if (anchoredPoints.Count < 2)
+                throw new Exception("Anchored points needs to be at least 2 to prevent rigid body motion!");
+
+            if (A.Count == 1)
+            {
+                List<double> iA_constValue = new List<double>();
+                for (int i = 0; i < lines.Count; i++)
+                    iA_constValue.Add(A[0]);
+                A = iA_constValue;
+            }
+            else if (A.Count != lines.Count)
+                throw new Exception("A is wrong size! Input list with same length as Lines or constant value!");
+
+
+            Elements = new List<Element>();
+            Nodes = new List<Point3d>();
+
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                Point3d startPoint = lines[i].PointAt(0);
+                Point3d endPoint = lines[i].PointAt(1);
+                Elements.Add(new Element(startPoint, endPoint, ref Nodes, anchoredPoints, E, A[i]));
+            }
+
+            int dofs = 2 * Nodes.Count;
+            
+            while (loadList.Count < dofs)
+                loadList.Add(0);
+
+            while (loadList.Count > dofs)
+                loadList.RemoveAt(loadList.Count - 1);
+
+
+            if (loadVecs.Count <= Nodes.Count)
+                for (int i = 0; i < loadVecs.Count; i++)
+                {
+                    loadList[2 * i] += loadVecs[i].X;
+                    loadList[2 * i + 1] += loadVecs[i].Y;
+                }
+
+            R0 = Vector<double>.Build.Dense(dofs);
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                R0[2 * i] = loadList[2 * i];
+                R0[2 * i + 1] = loadList[2 * i + 1];
+            }
+
+            K = Matrix<double>.Build.Dense(dofs, dofs);           
+            r = Vector<double>.Build.Dense(dofs);
+            N = Vector<double>.Build.Dense(0);
+
+            K_out = new Matrix(dofs, dofs);
+            r_out = new Matrix(dofs, 1);
+            //N_out = new Matrix(Elements.Count, 1);
+            N_out = new List<double>();
+
+        }
+
+
+
+
+        public void Assemble()
+        {
+            foreach (Element element in Elements)
+            {
+                double dy = element.EndPoint.Y - element.StartPoint.Y;
+                double dx = element.EndPoint.X - element.StartPoint.X;
+                double rad = Math.Atan2(dy, dx);                            
+                double EAL = (element.A * element.E / element.Length());
+                double cc = Math.Cos(rad)*Math.Cos(rad)*EAL;
+                double ss = Math.Sin(rad)*Math.Sin(rad)*EAL;
+                double sc = Math.Sin(rad)*Math.Cos(rad)*EAL;
+
+                int startIndex = element.StartNodeIndex;
+                int endIndex = element.EndNodeIndex;
+
+                if (startIndex != -1)
+                {
+                    K[2 * startIndex, 2 * startIndex] += cc;
+                    K[2 * startIndex + 1, 2 * startIndex + 1] += ss;
+                    K[2 * startIndex + 1, 2 * startIndex] += sc;
+                    K[2 * startIndex, 2 * startIndex + 1] += sc;
+                }
+                if (endIndex != -1)
+                {
+                    K[2 * endIndex, 2 * endIndex] += cc;
+                    K[2 * endIndex + 1, 2 * endIndex + 1] += ss;
+                    K[2 * endIndex + 1, 2 * endIndex] += sc;
+                    K[2 * endIndex, 2 * endIndex + 1] += sc;
+                }
+                if (startIndex != -1 && endIndex != -1)
+                {
+                    K[2 * startIndex, 2 * endIndex] += -cc;
+                    K[2 * startIndex + 1, 2 * endIndex + 1] += -ss;
+                    K[2 * startIndex + 1, 2 * endIndex] += -sc;
+                    K[2 * startIndex, 2 * endIndex + 1] += -sc;
+
+                    K[2 * endIndex, 2 * startIndex] += -cc;
+                    K[2 * endIndex + 1, 2 * startIndex + 1] += -ss;
+                    K[2 * endIndex + 1, 2 * startIndex] += -sc;
+                    K[2 * endIndex, 2 * startIndex + 1] += -sc;
+                }
+                
+            }
+        }
+
+
+
+
+        public void Solve()
+        {
+            r = K.Solve(R0);
+
+            for (int i = 0; i < Nodes.Count; i++)
+                Nodes[i] += new Point3d(r[2 * i], r[2 * i + 1], 0);
+
+            for (int i = 0; i < r.Count; i++)
+                r_out[i, 0] = r[i];
+
+            for (int i = 0; i < K.RowCount; i++)
+            {
+                for (int j = 0; j < K.ColumnCount; j++)
+                    K_out[i, j] = K[i, j];
+            }
+        }
+
+
+
+
+        public void Retracking()
+        {
+            foreach (Element element in Elements)
+            {
+                double L0 = element.Length();
+                double L1 = 0;
+
+                if (element.StartNodeIndex == -1 && element.EndNodeIndex == -1)
+                    L1 = L0;                   
+                else if(element.EndNodeIndex == -1)
+                    L1 = Nodes[element.StartNodeIndex].DistanceTo(element.EndPoint);
+                else if(element.StartNodeIndex == -1)
+                    L1 = element.StartPoint.DistanceTo(Nodes[element.EndNodeIndex]);
+                else
+                    L1 = Nodes[element.StartNodeIndex].DistanceTo(Nodes[element.EndNodeIndex]);
+
+                double N_element = (element.A * element.E / L0) * (L1 - L0);
+                N.Append(N_element);
+                //N_out[count++, 0] = N_element;
+                N_out.Add(N_element);
+            }
+        }
+
+
+        public void ResultVisuals(out List<System.Drawing.Color> colors)
+        {
+            //Interval N_interval = new Interval(N.Min(), N.Max());
+
+            double min = N_out[0];
+            double max = N_out[0];
+
+            double f_dim = 355;
+            List<double> sigma = new List<double>();
+            List<double> utilization = new List<double>();
+            colors = new List<System.Drawing.Color>();
+
+            for (int i = 1; i < Elements.Count; i++)
+            {
+                if (N_out[i] < min)
+                    min = N_out[i];
+                if (N_out[i] > max)
+                    max = N_out[i];
+            }
+
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                sigma.Add( N_out[i] / Elements[i].A );
+                utilization.Add(sigma[i] / f_dim);
+                
+                N_out[i] = utilization[i];
+
+                
+                int r = 0;
+                int g = 0;
+                int b = 0;
+                int alfa = 255;
+
+                if (utilization[i] > 1 || utilization[i] < -1)
+                {
+                    r = 255;
+                    g = 0;
+                    b = 0;
+                }
+                else
+                {
+                    r = 0;
+                    g = 255;
+                    b = 0;
+                }
+
+                colors.Add(System.Drawing.Color.FromArgb(alfa,r,g,b));
+            }                
+        }
+
+
+
+        public string PrintInfo()
+        {
+            string info = "";
+            foreach (Element element in Elements)
+                info += element.elementInfo + "\n";
+            return info;
+        }      
+
+
+    }
+}
---
+++ MasterthesisGHA/Components/2DTruss.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+
+
+namespace MasterthesisGHA.Components
+{
+    public class Truss2D : GH_Component
+    {
+
+        public Truss2D()
+          : base("2D Truss Analysis", "2D Truss",
+              "Minimal Finite Element Analysis of 2D Truss",
+              "Master", "FEA")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddLineParameter("Input Lines", "Lines", "Lines for creating truss geometry", GH_ParamAccess.list);
+            pManager.AddPointParameter("Input Anchored Points", "Anchored", "Support points restricted from translation but free to rotate", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Cross Section Area [mm^2]", "A", "Cross Section Area by indivial member values (list) or constant value", GH_ParamAccess.list, 10000);
+            pManager.AddNumberParameter("Young's Modulus [N/mm^2]", "E", "Young's Modulus for all members", GH_ParamAccess.item, 210e3);
+            pManager.AddNumberParameter("Nodal Loading [N]", "Load", "Nodal loads by numeric values (x1, y1, x2, y2, ..)", GH_ParamAccess.list, new List<double> { 0 });
+            pManager.AddVectorParameter("Nodal Loading [N]", "vecLoad", "Nodal loads by vector input", GH_ParamAccess.list, new Vector3d(0,0,0));
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("#Elements", "#Elements", "#Elements", GH_ParamAccess.item);
+            pManager.AddNumberParameter("#Nodes", "#Nodes", "#Nodes", GH_ParamAccess.item);
+            pManager.AddMatrixParameter("K", "K", "K", GH_ParamAccess.item);
+            pManager.AddMatrixParameter("r", "r", "r", GH_ParamAccess.item);
+            pManager.AddPointParameter("Nodes", "Nodes", "Nodes", GH_ParamAccess.list);
+            //pManager.AddLineParameter("LoadVisual", "LoadVisual", "LoadVisual", GH_ParamAccess.list);
+            pManager.AddTextParameter("Info", "Info", "Info", GH_ParamAccess.item);
+            //pManager.AddMatrixParameter("N", "N", "N", GH_ParamAccess.list);
+            pManager.AddNumberParameter("N", "N", "N", GH_ParamAccess.list);
+            pManager.AddColourParameter("Util", "Util", "Util", GH_ParamAccess.list);
+        }
+
+
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // INPUT
+
+            List<Line> iLines = new List<Line>();
+            List<Point3d> iAnchoredPoints = new List<Point3d>();
+            List<double> iA = new List<double>();
+            double iE = 0;
+            List<double> iLoad = new List<double>();
+            List<Vector3d> iLoadVecs = new List<Vector3d>();
+                      
+            DA.GetDataList(0, iLines);
+            DA.GetDataList(1, iAnchoredPoints);
+            DA.GetDataList(2, iA);
+            DA.GetData(3, ref iE);
+            DA.GetDataList(4, iLoad);
+            DA.GetDataList(5, iLoadVecs);
+           
+
+            // CODE
+
+            TrussModel2D truss2D = new TrussModel2D(iLines, iA, iAnchoredPoints, iLoad, iLoadVecs, iE);
+            truss2D.Assemble();          
+            truss2D.Solve();
+            truss2D.Retracking();
+
+            truss2D.ResultVisuals(out List<System.Drawing.Color> colors_out);
+            
+            // OUTPUT
+
+            DA.SetData("#Elements", truss2D.Elements.Count);
+            DA.SetData("#Nodes",truss2D.Nodes.Count); 
+            DA.SetData("K", truss2D.K_out);
+            DA.SetData("r", truss2D.r_out);
+            DA.SetDataList("Nodes", truss2D.Nodes);
+            //DA.SetDataList("LoadVisual", truss2D.loadVisuals); 
+            DA.SetData("Info", truss2D.PrintInfo());
+            //DA.SetData("N", truss2D.N_out);
+            DA.SetDataList("N", truss2D.N_out);
+            DA.SetDataList("Util", colors_out);
+        }
+
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                return Properties.Resources._2D_Truss_Icon;
+                //return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1AB4E987-0B6A-4EFA-BD27-8418F7C24308"); }
+        }
+
+    }
+}
+
+
+
+
+
+
